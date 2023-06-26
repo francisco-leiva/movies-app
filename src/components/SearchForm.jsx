01edc643560c2109e387e8f3b7fb0a57b958595b
francisco-leiva <leivafrancisco631@gmail.com>
2023-06-26 15:32:03 -0300

    Change form styles

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -20,18 +20,18 @@ export default function SearchForm() {
 
   return (
     <form
-      className='max-w-3xl relative mx-3 mt-2 pb-3 md:mx-auto'
+      className='max-w-3xl flex mx-3 mt-2 pb-3 md:mx-auto'
       onSubmit={handleSubmit}
     >
       <input
         id='search'
-        className='w-full p-2 pr-[3.25rem] rounded-lg focus:outline-none'
+        className='w-full h-9 indent-3 rounded-l-lg focus:outline-none'
         type='text'
         placeholder='Search'
         onChange={handleChange}
       />
 
-      <button className='absolute top-0 right-0 px-2 py-1 text-black bg-[#2892D7] rounded-r-lg hover:bg-[#1D70A2]'>
+      <button className='px-2 text-black bg-[#2892D7] rounded-r-lg hover:bg-[#1D70A2]'>
         <Link href={`/search/${searchKey}`}>
           <SearchIcon />
         </Link>
