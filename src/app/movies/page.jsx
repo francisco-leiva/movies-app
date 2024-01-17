a2ebf6450d2ad38a1b41628ca76e21f2f8ffcb5e
francisco-leiva <leivafrancisco631@gmail.com>
2024-01-17 17:38:10 -0300

    style: change main page styles

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -6,7 +6,7 @@ export default async function Movies() {
 
   return (
     <main className='pt-24 pb-4'>
-      <section className='max-w-4xl w-full px-2 grid grid-cols-cards justify-items-center gap-x-4 gap-y-4 sm:px-0 md:mx-auto'>
+      <section className='max-w-4xl min-h-[calc(100vh-10rem)] w-full px-2 grid grid-cols-cards justify-items-center gap-x-4 gap-y-4 sm:px-0 md:mx-auto'>
         {popularMovies.map(({ id, title, posterPath }) => {
           return (
             <MovieCards key={id} id={id} image={posterPath} title={title} />
