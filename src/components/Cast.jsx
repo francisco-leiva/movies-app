454e550d2d8db361ef7a8bd9d690775f6f37935d
francisco-leiva <leivafrancisco631@gmail.com>
2024-01-17 18:46:24 -0300

    feat: add toggle switch to cast component to show all actors

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,12 +1,36 @@
+'use client'
+
+import { useState } from 'react'
 import CastCard from './CastCard'
 
 export default function Cast({ cast }) {
-  return (
-    <section className='max-w-5xl mx-3 px-2 bg-slate-100 lg:mx-auto'>
-      <h2 className='py-4 text-3xl font-bold uppercase'>Actors</h2>
+  const [showAll, setShowAll] = useState(false)
 
-      <div className='flex gap-6 flex-nowrap overflow-x-auto overflow-y-hidden lg:mx-3'>
-        {cast.map(({ id, name, character, profilePath }) => {
+  const actors = showAll ? cast : cast.slice(0, 6)
+
+  const toggleShowAll = () => setShowAll(!showAll)
+
+  return (
+    <section className='max-w-5xl mx-3 px-2 pb-4 bg-slate-100 lg:mx-auto'>
+      <div className='flex justify-between py-4'>
+        <h2 className='text-3xl font-bold uppercase'>Actors</h2>
+
+        <label className='inline-flex items-center cursor-pointer'>
+          <input
+            type='checkbox'
+            value=''
+            className='sr-only peer'
+            onChange={toggleShowAll}
+          />
+          <div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className='ms-2 text-base font-medium text-gray-900'>
+            Show all
+          </span>
+        </label>
+      </div>
+
+      <div className='flex gap-6 flex-nowrap overflow-x-auto overflow-y-hidden md:flex-wrap lg:mx-3'>
+        {actors.map(({ id, name, character, profilePath }) => {
           return (
             <CastCard
               key={id}
