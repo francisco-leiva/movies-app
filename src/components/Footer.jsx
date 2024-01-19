6857daef4f8f53ea1f96d6d58637c2ddd0094c92
francisco-leiva <leivafrancisco631@gmail.com>
2024-01-19 17:22:40 -0300

    style: change background color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className='w-full h-16 flex bg-[#1B264F] text-white'>
+    <footer className='w-full h-16 flex bg-blue-950 text-white'>
       <div className='max-w-4xl w-full mx-4 flex justify-between items-center text-xl lg:mx-auto'>
         <span>
           <Link href={'https://github.com/francisco-leiva'} target='_blank'>
@@ -21,5 +21,5 @@ export default function Footer() {
         </span>
       </div>
     </footer>
-  );
+  )
 }
