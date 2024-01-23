1edb488cc0fe10db16c27f7cfd9b2d2b03f538fd
francisco-leiva <leivafrancisco631@gmail.com>
2024-01-23 18:22:20 -0300

    refactor: add aria-label to main title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,9 @@ export default function Header() {
       <section className='max-w-4xl relative h-full flex items-center justify-between px-2 sm:mx-auto lg:px-0'>
         <div className='flex items-center gap-6'>
           <h1 className='mainTitle h-10 text-3xl tracking-[.25rem] bg-titleGradient'>
-            <Link href={'/'}>CineFlix</Link>
+            <Link href={'/'} aria-label='Go home'>
+              CineFlix
+            </Link>
           </h1>
 
           <nav className='hidden sm:inline'>
