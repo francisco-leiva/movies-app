3eb27514fc05b527deddcb1689129d16c0f2bfcf
francisco-leiva <leivafrancisco631@gmail.com>
2024-03-19 16:57:17 -0300

    style: change loading style

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className='absolute top-0 left-0 h-full w-full z-50 bg-black bg-opacity-60 flex justify-center items-center'>
+    <div className='absolute top-0 left-0 h-full w-full z-50 bg-black bg-opacity-40 flex justify-center items-center'>
       <span className='w-12 h-12 border-4 border-solid border-white border-b-transparent rounded-full animate-spin'></span>
     </div>
   )
