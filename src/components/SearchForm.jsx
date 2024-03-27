cb8de7115c436d90743f3941473f8b8cff37dcbb
francisco-leiva <leivafrancisco631@gmail.com>
2024-03-27 12:16:08 -0300

    refactor: change input useState for useRef

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,71 +1,56 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { SearchIcon } from './Icons'
 
 export default function SearchForm() {
-  const [searchKey, setSearchKey] = useState('')
+  const inputRef = useRef()
   const [openSearch, setOpenSearch] = useState(false)
   const router = useRouter()
 
   // open search input
   const handleOpen = () => setOpenSearch(!openSearch)
-  // set search key
-  const handleChange = (e) => setSearchKey(e.target.value)
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (!searchKey) return
+    if (!inputRef.current.value) return
 
     setOpenSearch(false)
-    router.push(`/search?q=${searchKey}&page=1`)
+    router.push(`/search?q=${inputRef.current.value}&page=1`)
   }
 
   return (
-    <form className='h-9 sm:w-60' onSubmit={handleSubmit}>
-      {/* mobile search */}
-      <div className='sm:hidden'>
-        <button
-          type='button'
-          aria-label='Open search input'
-          onClick={handleOpen}
-        >
-          <SearchIcon />
-        </button>
+    <form className='h-9 sm:w-60 sm:flex' onSubmit={handleSubmit}>
+      <button
+        type='button'
+        aria-label='Open search input'
+        className='sm:hidden'
+        onClick={handleOpen}
+      >
+        <SearchIcon />
+      </button>
 
-        <div
-          className={`${
-            openSearch ? 'block' : 'hidden'
-          } absolute w-screen h-screen top-20 left-0 backdrop-blur-sm`}
-        >
-          <input
-            id='search'
-            className='w-full h-14 text-lg indent-3 border-t-2 border-b-2 border-solid border-y-gray-300 focus:outline-none'
-            type='text'
-            placeholder='Search'
-            onChange={handleChange}
-          />
-        </div>
-      </div>
-
-      {/* desktop search */}
-      <div className='hidden sm:flex'>
+      <div
+        className={`${
+          openSearch ? 'block' : 'hidden'
+        } absolute w-screen h-screen top-20 left-0 backdrop-blur-sm sm:w-48 sm:h-9 sm:inline-block sm:static sm:backdrop-blur-none`}
+      >
         <input
+          ref={inputRef}
           id='search'
-          className='w-4/5 h-9 rounded-l-full text-lg indent-3 focus:outline-none'
+          className='w-full h-14 text-lg indent-3 border-t-2 border-b-2 border-solid border-y-gray-300 focus:outline-none sm:h-9 sm:rounded-l-full sm:border-none'
           type='text'
           placeholder='Search'
-          onChange={handleChange}
         />
-
-        <button
-          type='submit'
-          className='w-1/5 px-2 bg-white rounded-r-full'
-          aria-label='Submit'
-        >
-          <SearchIcon />
-        </button>
       </div>
+
+      <button
+        type='submit'
+        className='hidden sm:inline sm:w-12 sm:px-2 sm:bg-white sm:rounded-r-full'
+        aria-label='Submit'
+      >
+        <SearchIcon />
+      </button>
     </form>
   )
 }
