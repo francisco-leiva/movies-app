6dca3133868eb1d367be32b2919ce24f8ef6f83f
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-18 16:53:32 -0300

    refactor: destructure search shows function

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -8,48 +8,42 @@ import NoResults from './components/NoResults'
 
 export default async function SearchPage({ searchParams }) {
   const { q, page } = searchParams
-  const searchResults = await searchShows(q, page)
+  const { searchResults, totalPages } = await searchShows(q, page)
 
   return (
     <main className='pt-24 pb-4'>
       <section className='max-w-4xl min-h-[calc(100vh-11rem)] w-full px-2 grid grid-cols-cards justify-items-center gap-x-4 gap-y-4 sm:px-0 md:mx-auto'>
-        {searchResults ? (
-          searchResults.results.map(
-            ({ id, title, posterPath, type }, index) => {
-              if (type === 'movie') {
-                return (
-                  <MovieCards
-                    key={id}
-                    id={id}
-                    image={posterPath}
-                    title={title}
-                    index={index}
-                  />
-                )
-              } else {
-                return (
-                  <TvCards
-                    key={id}
-                    id={id}
-                    image={posterPath}
-                    title={title}
-                    index={index}
-                  />
-                )
-              }
+        {searchResults.length ? (
+          searchResults.map(({ id, title, posterPath, type }, index) => {
+            if (type === 'movie') {
+              return (
+                <MovieCards
+                  key={id}
+                  id={id}
+                  image={posterPath}
+                  title={title}
+                  index={index}
+                />
+              )
+            } else {
+              return (
+                <TvCards
+                  key={id}
+                  id={id}
+                  image={posterPath}
+                  title={title}
+                  index={index}
+                />
+              )
             }
-          )
+          })
         ) : (
           <NoResults />
         )}
       </section>
 
-      {searchResults ? (
-        <CustomPagination
-          path={'search'}
-          query={q}
-          totalPages={searchResults.totalPages}
-        />
+      {searchResults.length ? (
+        <CustomPagination path={'search'} query={q} totalPages={totalPages} />
       ) : null}
     </main>
   )
