e7b5bc71cab18b8244ca3b3884008964b2889f04
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-25 12:55:37 -0300

    refactor: optimize search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,28 +1,16 @@
 'use client'
 
-import { useState, useRef } from 'react'
-import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { SearchIcon } from './Icons'
 
 export default function SearchForm() {
-  const inputRef = useRef()
   const [openSearch, setOpenSearch] = useState(false)
-  const router = useRouter()
 
   // open search input
   const handleOpen = () => setOpenSearch(!openSearch)
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (!inputRef.current.value) return
-
-    setOpenSearch(false)
-    router.push(
-      `/search?${new URLSearchParams({ q: inputRef.current.value, page: 1 })}`
-    )
-  }
 
   return (
-    <form className='h-9 sm:w-60 sm:flex' onSubmit={handleSubmit}>
+    <form action='/search' role='search' className='h-9 sm:w-60 sm:flex'>
       <button
         type='button'
         aria-label='Open search input'
@@ -38,10 +26,9 @@ export default function SearchForm() {
         } absolute w-screen h-screen top-20 left-0 backdrop-blur-sm sm:w-48 sm:h-9 sm:inline-block sm:static sm:backdrop-blur-none`}
       >
         <input
-          ref={inputRef}
-          id='search'
+          type='search'
+          name='q'
           className='w-full h-14 text-lg indent-3 border-t-2 border-b-2 border-solid border-y-gray-300 focus:outline-none sm:h-9 sm:rounded-l-full sm:border-none'
-          type='text'
           placeholder='Search'
         />
       </div>
